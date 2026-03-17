22434ddce77e0be4784ed6ca9f713f490f1938a8
tt9297979-droid <tt9297979@gmail.com>
2026-03-17 21:26:38 +0700

    Create Chart.js

---
+++ components/Chart.js
@@ -0,0 +1,8 @@
+export default function Chart(){
+  return (
+    <iframe
+      src="https://s.tradingview.com/widgetembed/?symbol=XAUUSD&interval=5&theme=dark"
+      style={{width:"100%", height:"400px", border:"none"}}
+    />
+  )
+}
