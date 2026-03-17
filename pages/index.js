37aa519c5081408728268b22cdaba04ad656fbf4
tt9297979-droid <tt9297979@gmail.com>
2026-03-17 21:25:47 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home(){
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>🔥 AI HEDGE FUND</h1>
+        <a href="/login">👉 Login</a>
+      </div>
+    </div>
+  )
+}
